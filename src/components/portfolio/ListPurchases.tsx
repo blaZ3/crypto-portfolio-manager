5ffede567701a74308305fa2f06ff96ace4073fd
Vivek A S <tellvivk@gmail.com>
2020-05-18 22:54:57 +0300

    WIP purchase list

--- src/components/portfolio/ListPurchases.tsx
+++ src/components/portfolio/ListPurchases.tsx
@@ -10,20 +10,40 @@ import {
 } from 'evergreen-ui'
 
 import {getPurchases} from '../../repositories/PortfolioRepo'
+import {Purchase} from '../../models'
 
 export default function ListPurchases(props : any) {
 
+    let [purchases,
+        setPurchases] = useState < Purchase[] > ([]);
+
     useEffect(() => {
-        if(props.coin !== undefined){
+        if (props.coin !== undefined) {
             getPurchases(props.coin).then((purchases) => {
-                console.log(purchases);
+                setPurchases(purchases);
             }).catch((err) => {
                 alert("Error fetching purchases");
             });
-        }        
+        }
     }, [props.coin]);
 
     return (
-        <Pane></Pane>
+        <Pane>
+            {purchases.map((purchase) => {
+                return <PurchaseItem purchase={purchase}/>
+            })}
+        </Pane>
+    )
+}
+
+function PurchaseItem(props : any) {
+    let [purchase] = useState(props.purchase);
+    return (
+        <Pane display="flex" flexDirection="row">
+            <Text margin={minorScale(2)}>{purchase.date}</Text>
+            <Text margin={minorScale(2)}>{purchase.amount}</Text>
+            <Text margin={minorScale(2)}>{purchase.price}</Text>
+            <Text margin={minorScale(2)}>{purchase.quantity}</Text>
+        </Pane>
     )
 }
